a7896d77e9c72d897fddff4daa2f7891d50db5a6
Xavionstar <lincpough@gmail.com>
2022-12-15 21:12:52 -0800

    added tomtom route api

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+var apiKey = "75SvdXNRf0JhAVpoP7d4AWv0kjI96GNa"
+var locations = "52.50931,13.42936:52.50274,13.43872"
+
+
+
+function fetchTomTomRoute() {
+   
+    var queryURL = `https://api.tomtom.com/routing/1/calculateRoute/${locations}/json?key=${apiKey}`
+    fetch(queryURL, {
+        method: "GET"
+    })
+    .then(function (response){
+        return response.json();
+    })
+    .then(function (data){
+    console.log(data.routes)
+    })
+   
+}
+fetchTomTomRoute()
+
+
+
