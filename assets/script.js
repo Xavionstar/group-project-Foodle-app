69f943be9379b3ea9b8a9bea73122e1d6dcb3788
Xavionstar <lincpough@gmail.com>
2022-12-19 20:28:26 -0800

    pulling down Andys working map code

--- assets/script.js
+++ assets/script.js
@@ -4,13 +4,20 @@ var longCategoryString = "german , creole-cajun , dutch , banquet rooms , bistro
 var categoryString = "german , israeli , jamaican , vegetarian , seafood , vietnamese , sichuan , chinese , japanese  , fusion , salad bar , spanish , ethiopian , taiwanese , doughnuts , iranian , canadian , american , french , afghan , swiss , crêperie , surinamese , egyptian , hungarian , barbecue , hot pot , hamburgers , mediterranean , latin american , tapas , british , mexican , asian (other) , buffet , sushi , mongolian , international , mussels , thai , venezuelan , chicken , soup , kosher , steak house , yogurt/juice bar , italian , korean , bosnian , bolivian , dominican , belgian , tunisian , english , pakistani , czech , hawaiian , tibetan , arabian , middle eastern , chilean , shanghai , polish , filipino , sudanese , armenian , burmese , brazilian  bulgarian , soul food , colombian , jewish , pizza , sicilian , organic , greek , finnish , african , syrian , caribbean , russian , grill , take away , fast food , australian , irish , pub food , fondue , lebanese , indonesian , danish , indian , western continental , peruvian , cambodian , snacks , swedish , ice cream parlor , slavic , turkish , argentinean , austrian , exotic , portuguese , moroccan , sandwich , cuban"
 // var apiKeyLinc = "75SvdXNRf0JhAVpoP7d4AWv0kjI96GNa"
 // var locations = "52.50931,13.42936:52.50274,13.43872"
-var passengerInitCoordinates = [""];
+var passengerInitCoordinates = [long, lat];
 let passengerMarker;
 var categories = categoryString.split(" , ")
 var limit = 100;
 var selectedCategories = [];
 var selectedCategoriesIDs = [];
 var validRestaurants = [];
+const map = tt.map({
+  key: '75SvdXNRf0JhAVpoP7d4AWv0kjI96GNa',
+  container: 'map',
+  center: passengerInitCoordinates,
+  zoom: 13
+});
+
 
 getLocation()
 
@@ -20,17 +27,33 @@ function createPassengerMarker(markerCoordinates, popup) {
     return new tt.Marker({ element: passengerMarkerElement }).setLngLat(markerCoordinates).setPopup(popup).addTo(map);
 }
 
-const map = tt.map({
-  key: '75SvdXNRf0JhAVpoP7d4AWv0kjI96GNa',
-  container: 'map',
-  center: passengerInitCoordinates,
-  zoom: 13
-});
+
 
 passengerMarker = createPassengerMarker(passengerInitCoordinates,
   new tt.Popup({ offset: 35 }).setHTML("Click anywhere on the map to change passenger location."));
   
   passengerMarker.togglePopup();
+
+//   function drawPassengerMarkerOnMap(geoResponse) {
+//     if (geoResponse && geoResponse.addresses
+//         && geoResponse.addresses[0].address.freeformAddress) {
+//         passengerMarker.remove();
+//         passengerMarker = createPassengerMarker(geoResponse.addresses[0].position,
+//             new tt.Popup({ offset: 35 }).setHTML(geoResponse.addresses[0].address.freeformAddress));
+//         passengerMarker.togglePopup();
+//     }
+//   }
+
+//   map.on('click', function (event) {
+//     const position = event.lngLat;
+//     tt.services.reverseGeocode({
+//         key: apiKey,
+//         position: position
+//     })
+//         .then(function (results) {
+//             drawPassengerMarkerOnMap(results);
+//         });
+//   });
 
 //hides page 2 and 3 on website load
 $(function () {
@@ -125,6 +148,7 @@ function setPosition(position) {
   long = position.coords.longitude;
   passengerInitCoordinates.push(lat)
   passengerInitCoordinates.push(long)
+  
   console.log("Latitude: " + lat + "<br>Longitude: " + long);
   console.log(selectedCategories)
   getNearbyRestaurants();
@@ -227,14 +251,3 @@ $("#restart").click(function () {
 //     console.log(data.routes)
 //     })
    
-createPassengerMarker()
-
-function createPassengerMarker(markerCoordinates, popup) {
-    const passengerMarkerElement = document.createElement('div');
-    passengerMarkerElement.innerHTML = "<img src='img/man-waving-arm_32.png' style='width: 30px; height: 30px';>";
-    return new tt.Marker({ element: passengerMarkerElement }).setLngLat(markerCoordinates).setPopup(popup).addTo(map);
-}
-passengerMarker = createPassengerMarker(passengerInitCoordinates,
-  new tt.Popup({ offset: 35 }).setHTML("Click anywhere on the map to change passenger location."));
-  
-  passengerMarker.togglePopup();
