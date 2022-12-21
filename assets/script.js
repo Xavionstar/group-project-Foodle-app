749447187ebb4fa34427d93f54b1222f7465d984
Andy Abebaw <andyabebaw1@gmail.com>
2022-12-20 23:55:04 -0800

    add routes

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,7 @@ var apiKey = "BFfpEOBgB8gduVen0dobSrMbxnYQiYUG";
 var lat, long;
 var longCategoryString = "german , creole-cajun , dutch , banquet rooms , bistro , israeli , slovak , jamaican , vegetarian , seafood , vietnamese , maltese , sichuan , welsh , chinese , japanese , algerian , californian , fusion , shandong , salad bar , savoyan , spanish , ethiopian , taiwanese , doughnuts , iranian , canadian , american , norwegian , french , hunan , polynesian , afghan , roadside , oriental , swiss , erotic , crêperie , surinamese , egyptian , hungarian , nepalese , barbecue , hot pot , hamburgers , mediterranean , latin american , tapas , british , mexican , guangdong , asian (other) , buffet , sushi , mongolian , international , mussels , thai , venezuelan , rumanian , chicken , soup , kosher , steak house , yogurt/juice bar , italian , korean , cypriot , bosnian , bolivian , dominican , belgian , tunisian , scottish , english , pakistani , czech , hawaiian , maghrib , tibetan , arabian , middle eastern , chilean , shanghai , polish , filipino , sudanese , armenian , burmese , brazilian , scandinavian , bulgarian , soul food , colombian , jewish , pizza , sicilian , organic , greek , basque , uruguayan , cafeterias , finnish , african , corsican , syrian , caribbean , dongbei , russian , grill , take away , fast food , australian , irish , pub food , fondue , lebanese , indonesian , danish , provençal , teppanyakki , indian , mauritian , western continental , peruvian , cambodian , snacks , swedish , macrobiotic , ice cream parlor , slavic , turkish , argentinean , austrian , exotic , portuguese , luxembourgian , moroccan , sandwich , cuban"
 var categoryString = "german , israeli , jamaican , vegetarian , seafood , vietnamese , sichuan , chinese , japanese  , fusion , salad bar , spanish , ethiopian , taiwanese , doughnuts , iranian , canadian , american , french , afghan , swiss , crêperie , surinamese , egyptian , hungarian , barbecue , hot pot , hamburgers , mediterranean , latin american , tapas , british , mexican , asian (other) , buffet , sushi , mongolian , international , mussels , thai , venezuelan , chicken , soup , kosher , steak house , yogurt/juice bar , italian , korean , bosnian , bolivian , dominican , belgian , tunisian , english , pakistani , czech , hawaiian , tibetan , arabian , middle eastern , chilean , shanghai , polish , filipino , sudanese , armenian , burmese , brazilian  bulgarian , soul food , colombian , jewish , pizza , sicilian , organic , greek , finnish , african , syrian , caribbean , russian , grill , take away , fast food , australian , irish , pub food , fondue , lebanese , indonesian , danish , indian , western continental , peruvian , cambodian , snacks , swedish , ice cream parlor , slavic , turkish , argentinean , austrian , exotic , portuguese , moroccan , sandwich , cuban"
+var currentRestaurant = "";
 // var apiKeyLinc = "75SvdXNRf0JhAVpoP7d4AWv0kjI96GNa"
 // var locations = "52.50931,13.42936:52.50274,13.43872"
 
@@ -183,7 +184,8 @@ function addOnClickToCards(i) {
       marker.remove();
     }
     marker = new tt.Marker().setLngLat(restaurantCoordinates).addTo(map);
-    getDirections(restaurantCoordinates);
+   
+    getDirections(restaurantCoordinates, validRestaurants[i].poi.name);
     
     
     // var popup = new tt.Popup({offset: popupOffsets}).setHTML(validRestaurants[i].poi.name);
@@ -192,16 +194,49 @@ function addOnClickToCards(i) {
   });
 }
 
-function getDirections(restaurantCoordinates){
- var routesURL = "https://api.tomtom.com/routing/1/calculateRoute/" + lat  +"," + long + ":" + restaurantCoordinates[1] +"," + restaurantCoordinates[0] + "/json?key=75SvdXNRf0JhAVpoP7d4AWv0kjI96GNa";
+function getDirections(restaurantCoordinates, currRestaurant){
+  
+ tt.services.calculateRoute({
+  key: '75SvdXNRf0JhAVpoP7d4AWv0kjI96GNa',
+  traffic: false,
+  locations: long + "," + lat +":" + restaurantCoordinates[0] +"," + restaurantCoordinates[1]
+})
+  .then(function(response) {
+      var geojson = response.toGeoJson();
+      if (map.getLayer(currentRestaurant)) {
+      map.removeLayer(currentRestaurant);
+    }
+      currentRestaurant = currRestaurant;
+      map.addLayer({
+          'id': currentRestaurant,
+          'type': 'line',
+          'source': {
+              'type': 'geojson',
+              'data': geojson
+          },
+          'paint': {
+              'line-color': '#4a90e2',
+              'line-width': 8
+          }
+      });
+  });
+}
 
- fetch(routesURL)
- .then(function (response) {
-   return response.json();
- })
- .then(function (data) {
-   console.log(data);
- })
+function convertGeoJson (data) {
+  var geoJson = {
+    type: "FeatureCollection",
+    features: []
+};
+for (var i = 0; i < data.routes[0].legs[0].points.length; i++) {
+    geoJson.features.push({
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [data.routes[0].legs[0].points[i].longitude, data.routes[0].legs[0].points[i].latitude]
+        }
+    })
+}
+return [geoJson];
 }
 
 //hides page 1, and loads page 2 with the checkboxes
@@ -211,6 +246,16 @@ $("#startButton").click(openPageTwo);
 $("#getLocation").click(openPageThree);
 
 $("#restart").click(restart);
+
+$("#toggleExtraInfo").click(function () {
+  if($("#toggleExtraInfo").text() === "Show More"){
+    $("#toggleExtraInfo").text("Show Less");
+    $("#extraInfo").show();
+  } else {
+    $("#toggleExtraInfo").text("Show More");
+    $("#extraInfo").hide();
+  }
+});
 
 function openPageTwo(){
   $("#page1").hide();
@@ -222,6 +267,7 @@ function openPageThree(){
   getLocation();
   $("#page2").hide();
   $("#page3").show();
+  $("#extraInfo").hide();
   //add loading search text to page 3
   var loading = '<h5 id="loading"> Loading Search Results... </h5>';
   $("#options").append(loading);
@@ -245,6 +291,7 @@ function restart () {
   selectedCategories = [];
   validRestaurants = [];
 }
+
 
 function goBack(){
   restart();
